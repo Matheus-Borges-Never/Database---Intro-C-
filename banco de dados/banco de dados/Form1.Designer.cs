32d06c143b1b6a816ea75ea1edf4f2f292d4bf86
Matheus-Borges <84951351+Matheus-Borges-Never@users.noreply.github.com>
2022-06-02 16:18:25 -0300

    Add files via upload

---
+++ banco de dados/banco de dados/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace banco_de_dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtFone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtgCliente = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Teal;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(449, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir // Fechar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Teal;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(622, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(157, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Testar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.BackColor = System.Drawing.Color.Teal;
+            this.btnInserir.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnInserir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInserir.Location = new System.Drawing.Point(450, 155);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(156, 42);
+            this.btnInserir.TabIndex = 3;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = false;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(197, 123);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(227, 20);
+            this.txtNome.TabIndex = 4;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtFone
+            // 
+            this.txtFone.Location = new System.Drawing.Point(197, 179);
+            this.txtFone.Name = "txtFone";
+            this.txtFone.Size = new System.Drawing.Size(128, 20);
+            this.txtFone.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(89, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(89, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Telefone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(235, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(343, 42);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Deixe seu Contato";
+            // 
+            // dtgCliente
+            // 
+            this.dtgCliente.AllowUserToAddRows = false;
+            this.dtgCliente.AllowUserToDeleteRows = false;
+            this.dtgCliente.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dtgCliente.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dtgCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCliente.Location = new System.Drawing.Point(93, 238);
+            this.dtgCliente.Name = "dtgCliente";
+            this.dtgCliente.ReadOnly = true;
+            this.dtgCliente.Size = new System.Drawing.Size(686, 182);
+            this.dtgCliente.TabIndex = 9;
+            this.dtgCliente.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgCliente_CellContentClick);
+            this.dtgCliente.Click += new System.EventHandler(this.dtgCliente_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.Teal;
+            this.btnBuscar.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnBuscar.Location = new System.Drawing.Point(623, 155);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(156, 42);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dtgCliente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFone);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCliente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtFone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dtgCliente;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ banco de dados/banco de dados/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace banco_de_dados
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str = "";
+            str = "Data source = 10.64.45.32, 1433; Initial Catalog = TI41; User Id = aluno; Password = 123456";
+            con = new SqlConnection(str);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                MessageBox.Show("A conexão com o banco está aberta.");
+            } else if (con.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("A conexão com o banco está fechada.");
+            }
+            else 
+            {
+                MessageBox.Show("A conexão com o banco pode estar corrompida.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            else
+            {
+                con.Close();
+            }
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                SqlCommand cm;
+                string sql = "";
+                sql = "insert into matheus_cliente (nome,telefone) values (@NOME,@FONE)";
+                cm = new SqlCommand(sql, con);
+
+                cm.Parameters.Add("@NOME", SqlDbType.VarChar).Value = txtNome.Text;
+                cm.Parameters.Add("@FONE", SqlDbType.VarChar).Value = txtFone.Text;
+
+                int ret = cm.ExecuteNonQuery();
+                if (ret > 0)
+                {
+                    MessageBox.Show("O cliente foi inserido com sucesso!");
+                }
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                MessageBox.Show("A conexão com o banco está aberta.");
+            }
+            else if (con.State == ConnectionState.Closed)
+            {
+                MessageBox.Show("A conexão com o banco está fechada.");
+            }
+            else
+            {
+                MessageBox.Show("A conexão com o banco pode estar corrompida.");
+            }
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            SqlCommand cm;
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable tb = new DataTable();
+            string sql = "select * from matheus_cliente";
+            cm = new SqlCommand(sql, con);
+            da.SelectCommand = cm;
+            da.Fill(tb);
+            dtgCliente.DataSource = null;
+            dtgCliente.DataSource = tb;
+
+        }
+
+        private void dtgCliente_Click(object sender, EventArgs e)
+        {
+            if (dtgCliente.Rows.Count > 0)
+            {
+                txtNome.Text = dtgCliente.CurrentRow.Cells[1].Value.ToString();
+                txtFone.Text = dtgCliente.CurrentRow.Cells[2].Value.ToString();
+            }
+        }
+
+        private void dtgCliente_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
